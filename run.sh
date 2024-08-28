98d086e872f13140d98e97a851cc284e8de46718
SkjalgS <ssl0282uit.no>
2024-08-28 13:40:11 +0200

    :sparkles: Fixed wrap around

--- run.sh
+++ run.sh
@@ -22,24 +22,34 @@ if [ -z "$nodes" ]; then
 fi
 
 # Shuffle and select arg number of nodes
-random_nodes=$(echo "$nodes" | shuf -n "$1")
+shuffled_nodes=$(echo "$nodes" | shuf -n "$1")
+IFS=$'\n' read -r -d '' -a node_array <<< "$shuffled_nodes"
+
+# Get number of available nodes
+num_available_nodes=${#node_array[@]}
 
 # Initialize and format list
 node_list="["
 
-for node in $random_nodes; 
+for ((i=0; i<$1; i++));
 do
+    # Get node from array, wrap around if needed
+    node=${node_array[$((i % num_available_nodes))]}
+
     # Get random port
     port=$(shuf -i 49152-65535 -n1)
     nodePort="$node:$port"
 
     # Start server on node with random port
-    ##ssh -f $node "cd $PWD && python3 server.py $port" && echo "Server started on $node:$port"
+    ssh -f $node "cd $PWD && python3 server.py $port" && echo "Server started on $node:$port"
 
     # Addd node to node list
-    node_list+="'$nodePort',"
+    node_list+="\"$nodePort\","
 done
 
 # Format the node list
 node_list=${node_list::-1}
 node_list+="]"
+
+# Print the list
+echo "Node list: $node_list"
